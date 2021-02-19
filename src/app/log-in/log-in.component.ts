d72aecd5835e306287dbec9c73d1269b43bf534c
jakub_ant <jakub.antosiewicz.1998@gmail.com>
2021-02-19 15:07:13 +0100

    Submit functionality

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent implements OnInit {
+  logInForm!: FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+    this.logInForm = new FormGroup({
+      email: new FormControl('',[Validators.email, Validators.required ]),
+      password: new FormControl('',[Validators.minLength(8), Validators.required,] ),
+    })
+  }
+  onSubmit(){
+    const userData = {email: this.logInForm.controls.email.value, password:this.logInForm.controls.password.value}
+    console.log(userData)
   }
 
 }
